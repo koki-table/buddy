c1210b97c6ece7a46f8629e7500223ea8000753a
koki_s <76271374+koki_s@users.noreply.github.com>
2022-01-23 00:23:31 +0900

    画像修正

---
+++ assets/scripts/pages/pop.js
@@ -0,0 +1,7 @@
+// import { publicFn } from '.popmodule.js';
+
+
+// const element = document.querySelector('.motion');
+
+// tween({ to: 300, duration: 500 })
+//   .start((v) => element.style += 'translateX(' + v + 'px)');
